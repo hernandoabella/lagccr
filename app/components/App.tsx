dd5074d5bf210451bc56f3661739fe09d33d13a4
Hernando Abella <hernandoabella@gmail.com>
2024-06-27 18:29:10 -0500

    Create App.tsx

---
+++ app/components/App.tsx
@@ -0,0 +1,19 @@
+import Header from "@/app/components/Header";
+import Hero from "@/app/components/Hero";
+import Team from "@/app/components/Team";
+import Services from "@/app/components/Services";
+import Footer from "@/app/components/Footer";
+
+const app = () => {
+  return (
+    <div>
+      <Header />
+      <Hero />
+      <Team />
+      <Services />
+      <Footer />
+    </div>
+  );
+};
+
+export default app;
