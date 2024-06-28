13450734d9353f77d25640676922f36785c81a80
Hernando Abella <hernandoabella@gmail.com>
2024-06-27 20:23:55 -0500

    Update Hero.tsx

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,7 +1,21 @@
-import React from "react";
-
-const Hero = () => {
-  return <div>Hero</div>;
+const HeroSection = () => {
+  return (
+    <section className="py-20">
+      <div className="container mx-auto text-center">
+        <h1 className="font-bold py-3 text-5xl">Trade Facilitation</h1>
+        <h2 className="text-3xl font-bold mb-4">
+          Our organization makes it easy for your foreign trade operations,
+          market research, and bilateral relations.
+        </h2>
+        <a
+          href="https://calendly.com/lagccr/30min"
+          className="bg-teal-200 hover:bg-teal-400 py-3 px-8 rounded-lg text-lg font-semibold transition duration-300 inline-block"
+        >
+          Book an Appointment
+        </a>
+      </div>
+    </section>
+  );
 };
 
-export default Hero;
+export default HeroSection;
