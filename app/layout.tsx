f1086f12d9390731f774aac2e296e6c825c1e346
Hernando Abella <hernandoabella@gmail.com>
2024-06-27 20:23:57 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "LAGCCR",
+  title: "LAGIR",
   description: "International Trade",
 };
 
