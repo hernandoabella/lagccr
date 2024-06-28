986d334f015ef7f6c772587b26ff4d86f33666e7
Hernando Abella <hernandoabella@gmail.com>
2024-06-27 19:07:20 -0500

    Update Header.tsx

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,45 +1,36 @@
 const Header = () => {
-  return (
-    <header className="bg-gray-800 py-4">
-      <div className="container mx-auto flex items-center justify-between">
-        {/* Logo */}
-        <div className="flex items-center">
-          <img src="/logo.png" alt="LAGCCR Logo" className="h-12" />
+    return (
+      <header className="bg-white py-4">
+        <div className="container mx-auto flex flex-col items-center">
+          {/* Logo */}
+          <div className="mb-2">
+            <img src="/logo.png" alt="LAGCCR Logo" className="h-60" />
+          </div>
+  
+          {/* Navegación */}
+          <nav>
+            <ul className="flex space-x-4">
+              <li>
+                <a href="#" className="text-gray-800 hover:text-gray-600">Inicio</a>
+              </li>
+              <li>
+                <a href="#" className="text-gray-800 hover:text-gray-600">Sobre Nosotros</a>
+              </li>
+              <li>
+                <a href="#" className="text-gray-800 hover:text-gray-600">Servicios</a>
+              </li>
+              <li>
+                <a href="#" className="text-gray-800 hover:text-gray-600">Equipo</a>
+              </li>
+              <li>
+                <a href="#" className="text-gray-800 hover:text-gray-600">Contacto</a>
+              </li>
+            </ul>
+          </nav>
         </div>
-
-        {/* Navegación */}
-        <nav className="hidden md:block">
-          <ul className="flex space-x-4">
-            <li>
-              <a href="#" className="text-white hover:text-gray-300">
-                Inicio
-              </a>
-            </li>
-            <li>
-              <a href="#" className="text-white hover:text-gray-300">
-                Sobre Nosotros
-              </a>
-            </li>
-            <li>
-              <a href="#" className="text-white hover:text-gray-300">
-                Servicios
-              </a>
-            </li>
-            <li>
-              <a href="#" className="text-white hover:text-gray-300">
-                Equipo
-              </a>
-            </li>
-            <li>
-              <a href="#" className="text-white hover:text-gray-300">
-                Contacto
-              </a>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    </header>
-  );
-};
-
-export default Header;
+      </header>
+    );
+  };
+  
+  export default Header;
+  
